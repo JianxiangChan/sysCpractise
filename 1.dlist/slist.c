7206ec70b228cadcd6e2c299c76d40b6f9eb7c79
JianxiangChan <15651898806@163.com>
2017-05-10 23:43:41 +0800

    add node head

--- 1.dlist/slist.c
+++ 1.dlist/slist.c
@@ -22,6 +22,7 @@ struct node * creat_node(int data)
 	p->pNext = NULL; 
 	return p;
 }
+
 void insert_tail(struct node *pH,struct node *new)
 {
 	//查找最后一个指针
@@ -34,13 +35,51 @@ void insert_tail(struct node *pH,struct node *new)
 	p->pNext = new;
 } 
 
-int main (void)
+void insert_head(struct node *pH,struct node *new)
 {
-	struct node *pHeader =creat_node(1);
+	new->pNext = pH->pNext;
+	pH->pNext = new;
+}
+
+void list_for_each(struct node* pH)
+{
+	struct node *p =pH;
+	printf("--------------begin-------------\n");
+	while(NULL != p->pNext)
+	{
+		p = p->pNext;
+		printf("node data: %d.\n",p->data);
+	}
+	printf("--------------end-------------\n");
+}
+
+int delete_node(struct node *pH, int data)
+{
+	struct node *p =pH;	//用来指向当前节点
+	struct node *pPREV = NULL;	//用来指向当前节点的前一个节点
+	while(NULL != p->pNext)		//遍历，走到尾节点处推出循环
+	{
+		prev = p;
+		p = p->pNext;
+		if(p->)
+	}
+}
+
+
+int main (void)
+{	
+	struct node *pHeader =creat_node(0);
+	insert_tail(pHeader,creat_node(1));
 	insert_tail(pHeader,creat_node(2));
 	insert_tail(pHeader,creat_node(3));
-	printf("node1 data: %d.\n",pHeader->data);
-	printf("node2 data: %d.\n",pHeader->pNext->data);
-	printf("node3 data: %d.\n",pHeader->pNext->pNext->data);
+	insert_head(pHeader,creat_node(4));
+	//printf("node1 data: %d.\n",pHeader->data);
+	
+	printf("node1 data: %d.\n",pHeader->pNext->data);
+	printf("node2 data: %d.\n",pHeader->pNext->pNext->data);
+	printf("node3 data: %d.\n",pHeader->pNext->pNext->pNext->data);
+	printf("node4 data: %d.\n",pHeader->pNext->pNext->pNext->pNext->data);
+	list_for_each(pHeader);
 	return 0;
 }
+
