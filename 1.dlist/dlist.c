bfd17056871134108f12eafd9ead92e1935494fb
JianxiangChan <15651898806@163.com>
2017-05-12 09:46:39 +0800

    add Makefile

--- 1.dlist/dlist.c
+++ 1.dlist/dlist.c
@@ -2,4 +2,32 @@
 #include "malloc.h"
 #include "stdlib.h"
 //C语言双链表的实现
-//时间
+//构建一个双链表节点类型
+struct node
+{
+		int data;
+		struct node *pPrev;
+		struct node *pNext;
+};
+
+struct node *creat_node (int data)
+{
+	struct node *p = (struct node *)malloc(sizeof(struct node));
+	if(NULL == P)
+	{
+		printf("malloc error.\n");
+		return NULL;
+	}
+	P->data  = data;
+	P->pPrev = NULL;
+	P->pNext = NULL;
+	return p;
+}
+
+
+int main (void)
+{
+	struct node *pHeader = creat_node(0);
+	
+	
+}
