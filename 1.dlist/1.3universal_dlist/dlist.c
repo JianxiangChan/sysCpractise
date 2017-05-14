ee23007b0da84df60894816ba2a4e1743658402e
JianxiangChan <15651898806@163.com>
2017-05-14 23:45:37 +0800

    finish dlist_delete

--- 1.dlist/1.3universal_dlist/dlist.c
+++ 1.dlist/1.3universal_dlist/dlist.c
@@ -1,14 +1,14 @@
 /********************************
-*Program Assignmert: 		实现通用双向链表
-*Author:							JianxiangChan
-*Date:								2017/5/13
+*Program Assignmert: 	实现通用双向链表
+*Author:				JianxiangChan
+*Date:					2017/5/13
 *Description:					
 *********************************/
 /*********************************
 *Source code in:
-					sysCpractise\1.dlist\1.3universal
+				sysCpractise\1.dlist\1.3universal
 *Function List:	
-*						dlist_create	实现一个Dlist的初始化
+*				dlist_create	实现一个Dlist的初始化
 *						
 *
 **********************************/
@@ -87,36 +87,14 @@ int data_dlist_check(DList *thiz, void  *data)
 }
 
 /**********************************
-*Function Name:	dlist_len
-*Purpose:				查看链表的长度
-*Params:				
-*							@DList *  thiz	 插座链表的对象
-*Return:				int 返回链表的长度值
-*Limitation			
-**********************************/
-int dlist_len(DList *thiz)
-{
-	return_val_if_fail(thiz != NULL,RET_FAULT);
-	int len = 0;
-	DListNode *dlistnode = NULL;
-	dlistnode = thiz->head;
-	while(dlistnode->head != NULL)
-	{
-		dlistnode = dlistnode->next;
-		len = len + 1;
-	}
-	return len;
-}
-
-/**********************************
 *Function Name:	dlist_add
-*Purpose:				给双向链表添加节点和数据
+*Purpose:		给双向链表添加节点和数据
 *Params:				
-*							@DList *  thiz	 添加节点的对象
-							@int			index 添加节点的位置
-							@void * 	data   添加的数据地址
-*Return:				Ret
-*Limitation			返回RET_OK RET_FAULT RET_OOM
+*				@DList *  thiz	 添加节点的对象
+				@int	  index  :>= 0链表的位置 <0 链表尾部
+				@void *   data   添加的数据地址
+*Return:		Ret
+*Limitation		返回RET_OK RET_FAULT RET_OOM
 **********************************/
 Ret dlist_add(DList *thiz, int index, void *data)
 {
@@ -137,7 +115,7 @@ Ret dlist_add(DList *thiz, int index, void *data)
 	}
 	node->data = data;
 	
-	dlistnode = thiz;
+	dlistnode = thiz->head;
 	//找到index节点位置,或者找到链表尾部
 	while((0 < index)&&(NULL != dlistnode->pNext) )
 	{
@@ -171,6 +149,18 @@ Ret dlist_add(DList *thiz, int index, void *data)
 	return RET_FAULT
 } 
 
+/**********************************
+*Function Name:	dlist_delete
+*Purpose:		给双向链表删除节点和数据
+*Params:				
+*				@DList *  thiz	 删除节点的对象
+				@int	  index  >0链表的位置 
+								 <0 链表尾部 
+								 =0 删除整个链表
+				@void *   data   添加的数据地址
+*Return:		Ret
+*Limitation		返回RET_OK RET_FAULT RET_OOM
+**********************************/
 Ret dlist_delete(DList *thiz, int index)
 {
 	DListNode *dlistnode = NULL;
@@ -182,9 +172,28 @@ Ret dlist_delete(DList *thiz, int index)
 		return RET_FAULT;
 	}
 	
-	dlistnode = thiz;
+	dlistnode = thiz->head;
+	
+	if(0 == index)
+	{
+		//删除整个链表
+		while(NULL == thiz->current->pPrev)
+		{
+			thiz->current = thiz->current->pPrev;
+			thiz->current->pNext->pPrev = NULL;
+			thiz->current->pNext->data = NULL;
+			free(thiz->current->pNext);
+			thiz->current->pNext = NULL;
+		}
+		free(thiz->current);
+		free(thiz->head);
+		thiz->current = NULL;
+		thiz->head = NULL;
+		free(thiz);
+		thiz = NULL;
+	}
 	//找到index节点位置,或者找到链表尾部
-	while((0 < index)&&(NULL != dlistnode->pNext) )
+	while((0 < index)&&(NULL != dlistnode->pNext))
 	{
 		dlistnode = dlistnode->pNext;
 		index--;
@@ -211,94 +220,47 @@ Ret dlist_delete(DList *thiz, int index)
 	else 
 	{
 		//尾部节点(只有头节点没有其他节点，也放在尾部节点里面)
-		thiz->current
-		return RET_OK
+		if(NULL != thiz->current->pPrev)
+		{
+			//说明不是头结点
+			thiz->current = dlistnode->pPrev;
+			thiz->current->pNext = NULL;
+			dlistnode->pPrev = NULL;
+			dlistnode->pNext = NULL;
+			free(dlistnode);
+			dlistnode = NULL;
+			return RET_OK;
+		}
+		else
+		{
+			printf("error:no tail node to delete.\n");
+			return RET_FAULT;
+		}
 	}
 	return RET_FAULT
 }
 
 
-/*
-//函数体之间使用空格
-//双向链表尾部插入
-static void node_insert_tail (Node *thiz, Node *new) //第一个参数为对象，并用thiz命名
-{
-	//第一步：找到链表的尾节点
-	Node *p = thiz;
-	while (NULL != p->pNext)
-	{
-		p -> pNext;
-	}
-	
-	//第二步：将新的节点插入到原来尾部节点的后面
-	p->pNext = new;
-	new->pPrev = p;
-}
 
-//双向链表头部插入
-static void note_insert_head_middle(Node *thiz, Node *new)
-{
-	new->pNext = thiz->pNext;								//新节点的next指针指向原来的节点1的地址
-	//只有表头节点不能让第一个节点指向新节点，这样会导致内存错误
-	if(NULL != thiz->pNext)		
-		thiz->pNext->pPrev = new;
-	thiz->pNext = new;
-	new->pPrev = thiz;
-	
-}
 
- //双向链表中间插入
-void insert_middle(struct node *thiz, struct node *new)
-{
-	struct node *p  = thiz;
-	struct node *temp = thiz->pNext;
-	p->pNext = new;
-	new->pPrev = p;
-	new->pNext = temp;
-	temp->pPrev = new;
-} 
+/**********************************
+*Function Name:	dlist_display
+*Purpose:		显示链表内容
+*Params:				
+*				@DList *  thiz	 显示链表的对象
+				@int	  index  <0 链表的位置 
+								 <0 链表尾部 
+								 =0 删除整个链表
+				@void *   data   添加的数据地址
+*Return:		Ret
+*Limitation		返回RET_OK RET_FAULT RET_OOM
+**********************************/
 
-//插入双向链表中的任意一个节点
-//合理使用空格 参数之间用空格 
-char note_insert_all(Node *thiz, Node *new, int index)
-{
-	//等号之间用空格
-	Node *p = thiz;
-	if(0 > index)
-	{	
-					//语句末的分号和前面内容不要加空格
-		printf("error: illegal input of index.\n");
-		return 0;
-	}
-	
-	//第一步，找到index节点
-	
-		//用括号分隔表达式，不要靠默认优先级来判断
-		while((index --) && (NULL != p->pNext))
-		{
-			//括号分隔if/while/for等语句的代码块，哪怕只有一行
-			p = p->pNext;
-		}
-		//用tab缩进，如果代码层次过多（超过三层），说明可能代码设计上出了问题
-		if((NULL == p->pNext) && (0 != (index+1)))
-		{
-			printf("error:index beyond the MAX size of the list.\n");
-			return 0;
-		}
-		else if((NULL != p->pNext) && (0 == (index+1)))
-		{
-			note_insert_head_middle(p,new);
-		}
-		else
-		{
-			node_insert_tail(p,new);
-		}
 
-	return 1;
-}
 
-//正向遍历
-static Node * note_list_all(Node *thiz)
+
+
+/* static Node * note_list_all(Node *thiz)
 {
 	
 	Node *p = thiz;
@@ -367,34 +329,7 @@ char note_list(Node *thiz, int index)
 	}
 	return 0;
 	
-}
+} */
 
-char note_delete(Node *thiz, int data)
-{
-	Node *p = thiz;
-	if(NULL == p)
-	return -1;
 
-	while(NULL != p->pNext)
-	{
-		p = p->pNext;
-		if(p->data == data)
-		{
-			if(NULL == p->pNext)
-			{
-				p->pPrev->pNext = NULL;
-			}
-			else
-			{
-				p->pPrev->pNext = p->pNext;
-				p->pNext->pPrev = p->pPrev;
-			}
-			free(p);
-			return 0;
-		}
-	}
-	printf("未找到要删除的节点.\n");
-	return -1;
-}
-*/
 
